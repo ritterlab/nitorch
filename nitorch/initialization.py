49c0c96b6479761118c5be0f6594e658fe9914d1
RoshanRane <rosh1992@gmail.com>
2022-05-30 16:27:14 +0200

    Fix for issue-1249807609

--- nitorch/initialization.py
+++ nitorch/initialization.py
@@ -1,96 +1,26 @@
 # Initialize weights
-from torch.nn import init, Conv3d, BatchNorm3d, Linear
+from torch.nn import init
 
-
-def xavier(x):
-    """Wrapper for torch.nn.init.xavier method.
-
-    Parameters
-    ----------
-    x : torch.tensor
-        Input tensor to be initialized. See torch.nn.init.py for more information
-
-    Returns
-    -------
-    torch.tensor
-        Initialized tensor
-
-    """
-    return init.xavier_normal_(x)
-
-
-def xavier_uniform(x):
-    """Wrapper for torch.nn.init.xavier_uniform method.
-
-    Parameters
-    ----------
-    x : torch.tensor
-        Input tensor to be initialized. See torch.nn.init.py for more information
-
-    Returns
-    -------
-    torch.tensor
-        Initialized tensor
-
-    """
-    return init.xavier_uniform_(x)
-
-
-def he(x):
-    """Wrapper for torch.nn.init.kaiming_normal_ method.
-
-    Parameters
-    ----------
-    x : torch.tensor
-        Input tensor to be initialized. See torch.nn.init.py for more information
-
-    Returns
-    -------
-    torch.tensor
-        Initialized tensor
-
-    """
-    return init.kaiming_normal_(x)
-
-
-def he_uniform(x):
-    """Wrapper for torch.nn.init.kaiming_uniform_ method.
-
-    Parameters
-    ----------
-    x : torch.tensor
-        Input tensor to be initialized. See torch.nn.init.py for more information
-
-    Returns
-    -------
-    torch.tensor
-        Initialized tensor
-
-    """
-    return init.kaiming_uniform_(x)
-
-
-def weights_init(m, func=he_uniform):
+def weights_init(m, func=init.kaiming_uniform_, debug_print=False):
     """Performs weight initialization for a layer.
-
     Parameters
     ----------
-    m
-        The layer which weights should be initialized.
-    func
-        The function to use to initialize weights.
-
+    m   :  The layer which weights should be initialized.
+    func:  The sampling function from torch.nn.init class
+           to use to initialize weights.
+           Some other examples: init.xavier_normal_, 
+           xavier_uniform_, kaiming_normal_
     Returns
     -------
-    m
-        Weight initialized layer.
-
-    """
-    if isinstance(m, Conv3d):
+    m:  Weight initialized layer.
+    """ 
+    if hasattr(m, 'weight'):
         func(m.weight.data)
         if m.bias is not None:
-            init.constant_(m.bias, 0)
-    elif isinstance(m, BatchNorm3d):
+            init.constant_(m.bias, 0.001)
+    # for layers like nn.BatchNorm3d, just reset the parameters
+    elif hasattr(m, 'reset_parameters') and callable(m.reset_parameters): 
         m.reset_parameters()
-    elif isinstance(m, Linear):
-        m.reset_parameters()
+    # else skip the layer and print out its name
+    elif debug_print and len(list(m.children()))==0:
+        print(f"weights_init:: skipping layer {m}")
